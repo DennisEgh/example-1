6db04ab8402c9b08633d686968864a49f64194cb
Dennis <year_2000_@hotmail.com>
2022-07-03 20:59:43 +0200

    Added rounds with their respective callouts + made a playgame function

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const playerSelection = "RoCk";
 
 const computerSelection = computerPlay();
 
+let computerScore = 0
+let playerScore = 0
+
 
 function computerPlay(){
     const myArr = ["Rock", "Paper","Scissors"];
@@ -16,23 +19,43 @@ function playRound(computerSelection, playerSelection){
     computerSelection = computerPlay().toLowerCase();
 
 if (computerSelection == playerSelection){
-    console.log("TIE GAME.")
+console.log(` TIED. `)
+
 } else if (
-(computerSelection == "rock" && playerSelection == "scissors") ||
-(computerSelection == "scissors" && playerSelection == "paper") ||
-(computerSelection == "paper" && playerSelection == "rock")
-){console.log("You Lose.")
+    (computerSelection == "rock" && playerSelection == "scissors") ||
+    (computerSelection == "scissors" && playerSelection == "paper") ||
+    (computerSelection == "paper" && playerSelection == "rock")
+)  {(computerScore = ++computerScore), playerScore < computerScore ? 
+    console.log(` ${computerScore} - ${playerScore} To Them...`) :
+     (playerScore == computerScore ? 
+     console.log(` ${computerScore} - ${playerScore} To Them... It's a TIE. `) :
+     console.log(` ${computerScore} - ${playerScore} To Them... `));
+
+
 } else if (
     (computerSelection == "scissors" && playerSelection == "rock") ||
     (computerSelection == "paper" && playerSelection == "scissors") ||
     (computerSelection == "rock" && playerSelection == "paper")
-){console.log("You Win.")};
-   
-
+)  {(playerScore = ++playerScore), playerScore > computerScore ?
+     console.log(` ${playerScore} - ${computerScore} To You.`) :
+    (playerScore == computerScore ? 
+    console.log(` ${playerScore} - ${computerScore} To You. It's a TIE. `) :
+    console.log(` ${playerScore} - ${computerScore} To You. `))
+}; 
 };
 
 
 function playGame(){
-    playRound(computerSelection, playerSelection);
+for (i = 0; i < 5; i++){
+ playRound(computerSelection, playerSelection)}; 
+
+if (i = 5 && computerScore == playerScore){
+console.log("It's a draw. Both Live To Fight Another Day.")
+} else if ( i = 5 && computerScore > playerScore){
+    console.log("The Machines Win... This Time.")
+} else if (i = 5 && playerScore > computerScore){
+    console.log("You win!... This Time.")
 };
-console.log(playGame());
+};
+
+playGame();
